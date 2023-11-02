f0f9d3ac5123d134393b0dcab71ffa2511f0251c
rafadias008 <123601507+rafadias008@users.noreply.github.com>
2023-11-02 16:20:36 +0000

    Criado menu e funcoes na .c e .h

--- funcoes.c
+++ funcoes.c
@@ -1,1 +1,58 @@
+#include "funcoes.h"
+#include "stdio.h"
+#include <stdlib.h>
+#include <string.h>
 
+struct tarefa {
+  char prioridade[3];
+  char categoria[10];
+  char descricao[100];
+  char status[13];
+  int indice;
+  int max_caracteres[20];
+};
+
+void menu() {
+
+  int x;
+
+  do {
+    printf("-----------------------------------");
+    printf("\n\nGerenciador de tarefas!\n\n");
+    printf("1 - Criar tarefas\n");
+    printf("2 - Deletar tarefa\n");
+    printf("3 - Listar tarefas\n");
+    printf("4 - Editar tarefas\n");
+    printf("5 - Encerrar gerenciador\n");
+
+    printf("\nEscolha uma opção: ");
+    scanf("%d", &x);
+    getchar();
+
+    switch (x) {
+
+    case 1: {
+      adicionar_tarefas();
+      break;
+    }
+    case 2: {
+      deletar_tarefas();
+      break;
+    }
+    case 3: {
+      listar_tarefas();
+      break;
+    }
+    case 5: {
+      printf("\nFinalizando...\n");
+      break;
+    }
+    default: {
+      printf("\nOpcao invalida\n");
+      break;
+    }
+    }
+
+  } while (x != 5);
+  return;
+}
