69211e0686d35a0539213be45cd54128ffdbe81c
rafadias008 <123601507+rafadias008@users.noreply.github.com>
2023-11-02 18:02:19 +0000

    Adicionado a função de criar tarefa e os contadores

--- funcoes.c
+++ funcoes.c
@@ -92,3 +92,92 @@ void menu() {
   return;
 }
 
+// Adiciona uma nova tarefa à lista
+void adicionar_tarefas() {
+
+  FILE *arquivo_tarefas;
+  struct tarefa tarefas;
+
+  // Abre o arquivo binário para leitura e escrita
+  arquivo_tarefas = fopen("tarefas.bin", "rb+");
+  if (arquivo_tarefas == NULL) {
+    // Se o arquivo não existir, tenta criar
+    arquivo_tarefas = fopen("tarefas.bin", "wb+");
+    if (arquivo_tarefas == NULL) {
+      printf("Erro ao abrir/criar o arquivo.\n");
+      return;
+    }
+  }
+
+  // Verificar o número de tarefas ja esta no maximo
+  int numero_tarefas = contar_tarefas();
+  if (numero_tarefas >= 100) {
+    printf("\nLimite de tarefas atingido (100 tarefasno máximo).\n");
+    return;
+  }
+
+  printf("\nCategorias: \n");
+  printf("1 - Educação\n2 - Saúde\n3 - Academia\n4 - Trabalho\n5 - Hobby\n");
+  printf("\nEscolha a categoria: ");
+  fgets(tarefas.categoria, sizeof(tarefas.categoria), stdin);
+  tarefas.categoria[strcspn(tarefas.categoria, "\n")] = '\0';
+
+
+  if (strcmp(tarefas.categoria, "1") == 0) {
+    strcpy(tarefas.categoria, "Educação");
+  } else if (strcmp(tarefas.categoria, "2") == 0) {
+    strcpy(tarefas.categoria, "Saúde");
+  } else if (strcmp(tarefas.categoria, "3") == 0) {
+    strcpy(tarefas.categoria, "Academia");
+  } else if (strcmp(tarefas.categoria, "4") == 0) {
+    strcpy(tarefas.categoria, "Trabalho");
+  } else if (strcmp(tarefas.categoria, "5") == 0) {
+    strcpy(tarefas.categoria, "Hobby");
+  } else {
+    printf("\nOpção inválida\n");
+    return;
+  }
+  while ((getchar()) != '\n');
+
+
+  printf("\nDigite a descricao da tarefa: ");
+  fgets(tarefas.descricao, sizeof(tarefas.descricao), stdin);
+  tarefas.descricao[strcspn(tarefas.descricao, "\n")] = '\0';
+  
+  printf("\nDigite a prioridade da tarefa (0-10): ");
+  scanf("%d", &tarefas.prioridade);
+  getchar();
+
+  printf("\nStatus: \n");
+  printf("1 - Em andamento\n2 - Concluida\n3 - Não iniciada\n");
+  printf("\nEscolha o status: ");
+  fgets(tarefas.status, sizeof(tarefas.status), stdin);
+  tarefas.status[strcspn(tarefas.status, "\n")] = '\0';
+
+  if (strcmp(tarefas.status, "1") == 0) {
+    strcpy(tarefas.status, "Em Andamento");
+  } else if (strcmp(tarefas.status, "2") == 0) {
+    strcpy(tarefas.status, "Concluida");
+  } else if (strcmp(tarefas.status, "3") == 0) {
+    strcpy(tarefas.status, "Não iniciada");
+  } else {
+    printf("\nOpção inválida\n");
+    return;
+  }
+  
+  tarefas.indice = obter_indice();
+  
+  // Abre o arquivo binário para escrita em modo de adição
+  arquivo_tarefas = fopen("tarefas.bin", "ab");
+  if (arquivo_tarefas == NULL) {
+    printf("Erro ao abrir o arquivo.\n");
+    return;
+  }
+
+  fwrite(&tarefas, sizeof(struct tarefa), 1, arquivo_tarefas);
+
+  fclose(arquivo_tarefas);
+
+  printf("\nNovo tarefa criada com sucesso!\n");
+}
+
