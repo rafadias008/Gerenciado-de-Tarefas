5cecab774d120a60c8f8c18a702cbd9fa6d699da
rafadias008 <123601507+rafadias008@users.noreply.github.com>
2023-11-11 19:20:44 +0000

    adicionado parametro de entrada na função de indice, e retirada a função de menu e contador de tarefas

--- funcoes.c
+++ funcoes.c
@@ -13,12 +13,11 @@ struct tarefa {
 };
 
 // Obtem o numero de indice para criação das tarefas
-int obter_indice() {
+int obter_indice(const char *tarefas_arquivo) {
   FILE *arquivo_tarefas = fopen("tarefas.bin", "rb");
   if (arquivo_tarefas == NULL) {
     return 1;
   }
-
   struct tarefa tarefas;
   int maior_indice = 0;
 
@@ -31,72 +30,6 @@ int obter_indice() {
   fclose(arquivo_tarefas);
 
   return maior_indice + 1;
-}
-
-// Limita a criação de 100 tarefas
-int contar_tarefas() {
-  FILE *arquivo_tarefas;
-  struct tarefa temp_tarefas;
-  int contador = 0;
-
-  arquivo_tarefas = fopen("tarefas.bin", "rb");
-  if (arquivo_tarefas == NULL) {
-    printf("Erro ao abrir o arquivo.\n");
-    return contador;
-  }
-
-  while (fread(&temp_tarefas, sizeof(temp_tarefas), 1, arquivo_tarefas)) {
-    contador++;
-  }
-
-  fclose(arquivo_tarefas);
-
-  return contador;
-}
-
-void menu() {
-
-  int x;
-
-  do {
-    printf("-----------------------------------");
-    printf("\n\nGerenciador de tarefas!\n\n");
-    printf("1 - Criar tarefas\n");
-    printf("2 - Deletar tarefa\n");
-    printf("3 - Listar tarefas\n");
-    printf("4 - Editar tarefas\n");
-    printf("5 - Encerrar gerenciador\n");
-
-    printf("\nEscolha uma opção: ");
-    scanf("%d", &x);
-    getchar();
-
-    switch (x) {
-
-    case 1: {
-      adicionar_tarefas();
-      break;
-    }
-    case 2: {
-      deletar_tarefas();
-      break;
-    }
-    case 3: {
-      listar_tarefas();
-      break;
-    }
-    case 5: {
-      printf("\nFinalizando...\n");
-      break;
-    }
-    default: {
-      printf("\nOpcao invalida\n");
-      break;
-    }
-    }
-
-  } while (x != 5);
-  return;
 }
 
 // Adiciona uma nova tarefa à lista
